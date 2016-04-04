da44ba28e28f3b7a66a24f269fec150f62608d36
Alfredo de la Fuente Briceño <alfredo.delafuente.b@gmail.com>
2016-04-03 20:13:30 -0500

    Update Camera.java

--- Camera.java
+++ Camera.java
@@ -1,26 +1,25 @@
+package RAYTRACER;
 import java.lang.Math;
 
 public class Camera 
 {
 	private float angle ; 
-	private float wh ; 
-	
-	public Camera(){} ; 
+	private float wh ;  
 	
 	public Camera ( float fov , int width , int height ) 
 	{
 		this.angle = ( float ) ( Math.tan( Math.PI * 0.5f * fov / 180.0f) );
 		this.wh = (float)(width) / (float)( height ) ;
-	};
+	}
 	
 	public float getAngle()
 	{
 		return this.angle ; 
-	};
+	}
 	
 	public float getWH( )
 	{
 		return this.wh ;
-	};
+	}
 
-};
+}
