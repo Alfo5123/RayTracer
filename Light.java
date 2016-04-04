92e77a41295b71b0ffb4a1d2399aae5f4b626c12
Alfredo de la Fuente Briceño <alfredo.delafuente.b@gmail.com>
2016-04-03 20:14:28 -0500

    Update Light.java

--- Light.java
+++ Light.java
@@ -1,3 +1,4 @@
+package RAYTRACER;
 import java.awt.Color;
 
 public class Light
@@ -5,21 +6,20 @@ public class Light
 	private Point3D pos ; 
 	private Color  col ; 
 	
-	public Light(){};
-	
 	public Light( Point3D pos , Color col )
 	{
 		this.pos = pos ; 
 		this.col = col ;
-	};
+	}
 	
 	public Point3D getPos ( )
 	{
 		return this.pos ;
-	};
+	}
 	
 	public Color getCol ( )
 	{
 		return this.col ;
-	};
+	}
 }
+
