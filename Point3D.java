9cda9632b34459136ce5198a4427e957cb3a3141
Alfredo de la Fuente Briceño <alfredo.delafuente.b@gmail.com>
2016-04-03 20:19:57 -0500

    Update Point3D.java

--- Point3D.java
+++ Point3D.java
@@ -1,3 +1,4 @@
+package RAYTRACER;
 
 public class Point3D
 {
@@ -5,27 +6,25 @@ public class Point3D
 	private float y ; 
 	private float z ; 
 	
-	public Point3D (){}; 
-	
 	public Point3D ( float x , float y , float z )
 	{
 		this.x = x ; 
 		this.y = y ; 
 		this.z = z ; 
-	};
+	}
 	
 	public float getX ( )
 	{
 		return this.x ; 
-	};
+	}
 	
 	public float getY ( )
 	{
 		return this.y ;
-	};
+	}
 	
 	public float getZ ( )
 	{
 		return this.z ;
-	};
+	}
 }
