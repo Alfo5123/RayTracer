0df5301123638bda45f4b386f3c359715f7ac1ca
Alfredo de la Fuente Briceño <alfredo.delafuente.b@gmail.com>
2016-04-03 20:21:39 -0500

    Update Ray.java

--- Ray.java
+++ Ray.java
@@ -1,30 +1,29 @@
+package RAYTRACER;
 
 public class Ray 
 {
 	private Point3D pos ; // Position 
 	private Vector  dir ; // Direction
 	
-	public Ray (){} ; 
-	
 	public Ray ( Point3D pos , Vector dir )
 	{
 		this.pos = pos ; 
 		this.dir = dir ; 
-	};
+	}
 	
 	public Point3D getPos ( )
 	{
 		return this.pos ; 
-	};
+	}
 	
 	public Vector getDir( )
 	{
 		return this.dir ;
-	};
+	}
 	
 	public void normalize()
 	{
-		this.dir.normalize();
-	};
+		this.dir = dir.normalize() ;
+	}
 
 }
