48b0d7feb1cb1fc4b6d394f94e8f214fbe88a734
Alfredo de la Fuente Briceño <alfredo.delafuente.b@gmail.com>
2016-04-10 01:23:23 -0500

    Update Plane.java

--- Plane.java
+++ Plane.java
@@ -5,52 +5,46 @@ public class Plane extends Shape
 	public Vector Normal ; 
 	public Point3D P ; 
 	
-	private float t ; 
-	private float EPS = 0.00001f ; 
-	
-	public Plane ( Vector Normal , Point3D P , Material material )
+	public Plane ( Vector Normal , Point3D P , Material Material )
 	{
 		this.Normal = Normal ; 
 		this.P = P ;  
-		this.material = material ; 
+		this.Material = Material ; 
 	}
 
 	@Override
-	public boolean Intersect(Ray ray) 
+	public IntersectionResult  Intersect(Ray ray) 
 	{
-		Vector v = new Vector (  ray.getPos() , this.P  ) ;
+		Vector v = new Vector (  ray.getPosition() , P  ) ;
 		ray.normalize();
-		//v.norm() ; 
-		this.Normal = this.Normal.normalize() ;
 		
-		float den = ray.getDir().dot(this.Normal) ; 
+		// Calculate normalized Normal Vector at the Point of Intersection
+		Normal = Normal.normalize() ;
+		
+		double den = ray.getDirection().dot( Normal ) ; 
 		
 		// Check if crossing the plane
-		if ( den < EPS ) 
+		if ( den < 0.00001f ) 
 		{
-			this.t = v.dot(this.Normal ) / den ;
-			return ( this.t >= 0 ) ; 
+			double t = v.dot( Normal ) / den ;
+			if ( t < 0 ) return new IntersectionResult( null, null, null ) ;
+			
+			// Calculate Point of Intersection
+			Point3D Intersection = new Point3D (    ray.getPosition().getX() + t * ray.getDirection().getX() ,
+								ray.getPosition().getY() + t * ray.getDirection().getY() , 
+								ray.getPosition().getZ() + t * ray.getDirection().getZ() ) ;	
+
+			// Slightly modify the intersection point to avoid precision issues
+			double bias = 0.01f ;
+			Intersection = new Point3D (  Intersection.getX() + bias * Normal.getX() ,
+						      Intersection.getY() + bias * Normal.getY() ,
+						      Intersection.getZ() + bias * Normal.getZ() ) ;
+			
+			return new IntersectionResult(Intersection, Normal, Material) ;
+			
 		}
 		
-		return false ;
+		return new IntersectionResult( null, null, null ) ;
 	}
 
-	@Override
-	public Vector getNormal(Point3D p)
-	{
-		 return this.Normal ; 
-	}
-
-	@Override
-	public float getInter() 
-	{
-		return this.t ; 
-	}
-
-	@Override
-	public Material getMaterial() 
-	{
-		return this.material;
-	}
-	
 }
