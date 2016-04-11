d741f5ebe43690aefdf1bd7b160e4f4edb6f0d27
Alfredo de la Fuente Briceño <alfredo.delafuente.b@gmail.com>
2016-04-11 01:26:39 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,4 +1,4 @@
-package RAYTRACER;
+package raytracer;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +10,7 @@ public class Test
 		int Width = 1200 ; 
 		int Height = 1200 ; 
 		
-		Camera camera = new Camera ( 40.0f , Width , Height ) ; 
+		Camera camera = new Camera ( 30.0f , Width , Height ) ; 
 	
 		List<Light> lights = new ArrayList<Light>() ; 
 		
@@ -21,222 +21,64 @@ public class Test
 						new Point3D( 30.0f , 50.0f, 20.0f) ,
 						new Color ( 1.0f , 1.0f , 1.0f ) 
 				) 
-	    ) ;
+	    	) ;
 		
 		
 		List<Shape> shapes = new ArrayList<Shape>() ;
-		/*
-		shapes.add
-		( 
-				new Sphere
-				( 		  
-					new Point3D (0.0f, 0.0f, -20.0f),
-	                1.0f,
-	                new Material
-		            (
-		                    new Color(0.01f , 0.02f, 0.09f ),
-		                    new Color(0.4f, 0.35f, 0.4f),
-		                    new Color(0.5f, 0.45f, 0.3f),
-		                    20.0f,
-		                    100.0f
-		            ) 
-		        )
-		);
-		
-		shapes.add
-		( 
-				new Sphere
-				( 		  
-					new Point3D (-2.0f, 0.0f, -20.0f),
-	                1.0f,
-	                new Material
-		            (
-		                    new Color( 0.1f, .02f, .02f ),
-		                    new Color(.8f, .5f, .2f),
-		                    new Color(1.0f, .2f, .2f),
-		                    20.0f,
-		                    80.0f
-		            ) 
-		        )
-		);
-		
-		shapes.add
-		( 
-				new Sphere
-				( 		  
-					new Point3D (-0.0f, 0.0f, -15.0f),
-	                1.0f,
-	                new Material
-		            (
-		                    new Color( 0.02f, .1f, .05f ),
-		                    new Color(.8f, .5f, .2f),
-		                    new Color(1.0f, .2f, .2f),
-		                    20.0f,
-		                    90.0f
-		            ) 
-		        )
-		);
-		*/
-		/*
-		shapes.add
-		( 
-				new Sphere
-				( 		  
-					new Point3D (0.5f, -1.5f, -5.0f),
-	                1.0f,
-	                new Material
-		            (
-		                    new Color( 0.12f, .1f, .35f ),
-		                    new Color(.8f, .5f, .2f),
-		                    new Color(1.0f, .8f, .2f),
-		                    20.0f,
-		                    60.0f
-		            ) 
-		        )
-		);
-		*/
-		
+
 		shapes.add
 		( 
 				new Sphere
 				( 		  
 					new Point3D (-1.5f, 1.0f, -10.0f),
-	                1.0f,
-	                new Material
-		            (
-		                    new Color( 0.12f, .1f, .35f ),
-		                    new Color(.3f, .5f, .2f),
-		                    new Color(1.0f, .8f, .2f),
-		                    20.0f,
-		                    40.0f
-		            ) 
-		        )
-		);
-		
-
-		shapes.add
-		(
-				new Triangle
-				(
-						new Point3D ( 1.0f , 1.0f ,  -4.0f ) ,
-						new Point3D ( 0.0f , 0.0f , -4.0f ) ,
-						new Point3D ( 0.0f , 1.0f , -5.0f ) ,
-						
-						new Material
-			            (
-			                    new Color(0.18f , 0.13f, 0.12f ),
-			                    new Color(0.6f, 0.5f, 0.4f),
-			                    new Color(0.2f, 0.3f, 0.2f),
-			                    10.0f,
-			                    70.0f
-			            ) 
-	
-				)
+	                		1.0f,
+			                new Material
+				        (
+				                    new Color( 0.12f, .1f, .35f ),
+				                    new Color(.3f, .5f, .2f),
+				                    new Color(1.0f, .8f, .2f),
+				                    40.0f
+				        ) 
+		        	)
 		);
 		
 		shapes.add
-		(
-				new Triangle
-				(
-						new Point3D ( 1.0f , 0.0f ,  -5.0f ) ,
-						new Point3D ( 0.0f , 0.0f , -4.0f ) ,
-						new Point3D ( 1.0f , 1.0f ,  -4.0f ) ,
-
-						new Material
-			            (
-			                    new Color(0.1f , 0.15f, 0.2f ),
-			                    new Color(0.3f, 0.5f, 0.4f),
-			                    new Color(0.3f, 0.1f, 0.2f),
-			                    10.0f,
-			                    50.0f
-			            ) 
-	
-				)
+		( 
+				new Sphere
+				( 		  
+					new Point3D (1.5f, 1.0f, -15.0f),
+	                		1.2f,
+	                		new Material
+		            		(
+			                    	new Color( 0.2f, .11f, .15f ),
+			                    	new Color(.4f, .35f, .32f),
+			                    	new Color(0.8f, .7f, .2f),
+			                    	40.0f
+		            		) 
+		        	)
 		);
-		
-		shapes.add
-		(
-				new Triangle
-				(
-						new Point3D ( 1.0f , 0.0f ,  -5.0f ) ,
-						new Point3D ( 1.0f , 1.0f ,  -4.0f ) ,
-						new Point3D ( 0.0f , 1.0f , -5.0f ) ,
-						
-
-						new Material
-			            (
-			                    new Color(0.1f , 0.15f, 0.2f ),
-			                    new Color(0.3f, 0.5f, 0.4f),
-			                    new Color(0.3f, 0.1f, 0.2f),
-			                    10.0f,
-			                    50.0f
-			            ) 
-	
-				)
-		);
-		
-		shapes.add
-		(
-				new Triangle
-				(
-						new Point3D ( 1.0f , 1.0f ,  -4.0f ) ,
-						new Point3D ( 0.0f , 0.0f , -4.0f ) ,
-						new Point3D ( 0.0f , 1.0f , -5.0f ) ,
-						
-
-						new Material
-			            (
-			                    new Color(0.1f , 0.15f, 0.2f ),
-			                    new Color(0.3f, 0.5f, 0.4f),
-			                    new Color(0.3f, 0.1f, 0.2f),
-			                    10.0f,
-			                    50.0f
-			            ) 
-	
-				)
-		);
-	
 
 		shapes.add
 		(
 				new Plane 
 				(
-						new Vector (  0.0f , 1.0f , 0.0f ) , 
-						new Point3D ( 0.0f , -1.0f , 0.0f ) ,
-						new Material
-			            (
-			                    new Color(0.12f , 0.11f, 0.12f ),
-			                    new Color(0.4f, 0.2f, 0.5f),
-			                    new Color(0.1f, 0.2f, 0.3f),
-			                    20.5f,
-			                    80.0f
-			            ) 
+					new Vector (  0.0f , 1.0f , 0.0f ) , 
+					new Point3D ( 0.0f , -1.0f , 0.0 ) ,
+					new Material
+				        (
+			                    	new Color(0.12f , 0.29f, 0.12f ),
+					        new Color(0.2f, 0.35f, 0.21f),
+					        new Color(0.1f, 0.4f, 0.3f),
+					        80.0f
+				        ) 
 	
 				)
 		);
-/*		
-		shapes.add
-		(
-				new Plane 
-				(
-						new Vector (  -1.0f , 0.0f , -0.5f ) , 
-						new Point3D ( 5.0f , 0.0f , 0.0f ) ,
-						new Material
-			            (
-			                    new Color(0.15f , 0.18f, 0.12f ),
-			                    new Color(0.4f, 0.55f, 0.5f),
-			                    new Color(0.1f, 0.2f, 0.3f),
-			                    20.5f,
-			                    80.0f
-			            ) 
 	
-				)
-		);
-		
-		
-		*/
+	
+	
 		Renderer test = new Renderer 
-	    (	
+	    	(	
 			new Scene ( camera , lights , shapes ), 	// Scene Generation
 			Width, // Width
 			Height ,  // Height
@@ -244,6 +86,6 @@ public class Test
 		) ;		
 		
 		
-		test.writeImage("Image_01");
+		test.writeImage("Image_07");
 	}
 }
